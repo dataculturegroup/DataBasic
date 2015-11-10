f01bc39849193c5ae0f27de1afd48757e5b6c5b4
Rahul Bhargava <rahulb@media.mit.edu>
2015-11-10 14:29:19 -0500

    change times to be unix timestamps

--- databasic/logic/db.py
+++ databasic/logic/db.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, json, logging, time
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from databasic import settings
@@ -17,24 +17,26 @@ class MongoHandler:
 			'ignore_case': ignore_case,
 			'ignore_stopwords': ignore_stopwords,
 			'title': str(title),
-			'datetime': datetime.datetime.now()
+			'created_at': time.time()
 			}))
 
 	def save_csv(self, collection, results):
 		return str(self._db[collection].save({
 			'results': results,
-			'datetime': datetime.datetime.now()
+			'created_at': time.time()
 			}))
 
 	def save_queued_files(self, collection, filepaths, filenames, is_sample_data, email, results_url_base):
-		doc_id = str(self._db[collection].save({
+		properties = {
 			'filepaths': filepaths,
 			'filenames': filenames,
 			'is_sample_data': is_sample_data,
 			'email': email,
 			'status': 'queued',
-			'datetime': datetime.datetime.now()
-			}))
+			'created_at': time.time()
+			}
+		logging.info(json.dumps(properties))
+		doc_id = str(self._db[collection].save(properties))
 		self.update_document(collection, doc_id, {'$set': {'results_url': results_url_base + doc_id}})
 		return doc_id
 
