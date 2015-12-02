76d57b43935ae95f05d3781dc6611360c30fe043
Rahul Bhargava <rahulb@media.mit.edu>
2015-12-01 20:01:12 -0500

    starting to add in logging

--- databasic/application.py
+++ databasic/application.py
@@ -1,4 +1,4 @@
-import logging, os
+import logging, os, sys, ntpath, logging.handlers
 from flask import Flask, Blueprint, g, redirect, request, abort
 from flask.ext.babel import Babel
 from flask_debugtoolbar import DebugToolbarExtension
@@ -8,22 +8,39 @@ from babel.support import LazyProxy
 from logic import oauth
 from logic.db import MongoHandler
 
-logging.basicConfig(level=logging.DEBUG)
+ENV_CONFIG_FILE_VAR = 'APP_CONFIG_FILE'	# the environment variable holding path to app config file
+ENV_CONFIG_FILE_VAR_MISSING_VAL = 'NOTSET' # the default val indicating the user needs to set the env config var
 
+basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# make sure config file is specified
+config_file_path = os.environ.get(ENV_CONFIG_FILE_VAR, ENV_CONFIG_FILE_VAR_MISSING_VAL)
+if config_file_path is ENV_CONFIG_FILE_VAR_MISSING_VAL:
+	print("ERROR! missing necessary environment variable %s" % ENV_CONFIG_FILE_VAR)
+	print("Set it with something like this and try again")
+	print("  export "+ENV_CONFIG_FILE_VAR+"=/abs/path/to/DataBasic/config/development.py")
+	sys.exit()
+
+# set up logging
+app_mode = os.path.splitext(ntpath.basename(config_file_path))[0]
+log_file_path = os.path.join(basedir,'logs', app_mode+'.log')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.RotatingFileHandler(log_file_path, maxBytes=5242880, backupCount=10)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.info("Starting DataBasic in %s mode" % app_mode)
+
+# Initialize the app
 app = Flask(__name__, instance_relative_config=False)
-
-# Load the default configuration
 app.config.from_object('config.default')
-
-# Load the configuration from the instance folder
 app.config.from_pyfile('../config/settings.py')
-
 # Load the file specified by the APP_CONFIG_FILE environment variable
 # Variables defined here will override those in the default configuration
-app.config.from_envvar('APP_CONFIG_FILE')
+app.config.from_envvar(ENV_CONFIG_FILE_VAR)
 
 # Setup sass auto-compiling
-basedir = os.path.dirname(os.path.abspath(__file__))
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
 	'databasic': ('static/sass', 'static/css', '/static/css')
 })
