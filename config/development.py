def11f12fdfd1ee48cb0637a704f3f039bb15bc5
Rahul Bhargava <rahulb@media.mit.edu>
2015-12-10 14:49:57 -0500

    betterdefaults

--- config/development.py
+++ config/development.py
@@ -1,7 +1,7 @@
-SECRET_KEY = 'auz-#clq8bx0+u0bhy8)(mkcgj3$-$07ooupvfo+!mcxx5fumj'
-MONGODB_URL = 'mongodb://user2devdb:data4dbdev!123@candidate.54.mongolayer.com:10871,candidate.3.mongolayer.com:11039/databasic-dev?replicaSet=set-56698ad2ef2cd4079a000f19'
+SECRET_KEY = 'SECRET_KEY'
+MONGODB_URL = 'mongodb://localhost/'
 MONGODB_NAME = 'databasic-dev'
-GOOGLE_CLIENT_ID = "1064885312187-risbbovaoejqrajohbof483l18jalq77.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "EnqniG8Yw-9Pdt7ZkjSbGNf4"
+GOOGLE_CLIENT_ID = "MY_CLIENT_ID"
+GOOGLE_CLIENT_SECRET = "MY_SECRET_ID"
 GOOGLE_ANALYTICS_ID = ''
 DEBUG = True
