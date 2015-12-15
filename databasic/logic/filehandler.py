927ac347839b9fedbb8dee97966554295806398d
Rahul Bhargava <rahulb@media.mit.edu>
2015-12-15 13:51:58 -0500

    handle tmp files dissapearing in wordcounter, also log while converting to txt

--- databasic/logic/filehandler.py
+++ databasic/logic/filehandler.py
@@ -83,13 +83,17 @@ def convert_to_txt(file_path):
     words = None
     ext = _get_extension(file_path)
     if ext == '.txt':
+        logger.debug("loading txt file")
         with codecs.open(file_path, 'r', ENCODING) as myfile:
             words = myfile.read()
     elif ext == '.docx':
+        logger.debug("loading docx file")
         words = _docx_to_txt(file_path)
     elif ext == '.rtf':
+        logger.debug("loading rtf file")
         doc = Rtf15Reader.read(open(file_path))
         words = PlaintextWriter.write(doc).getvalue()
+    logger.debug("loaded %d chars" % len(words))
     return words
 
 def convert_to_csv(file_path):
