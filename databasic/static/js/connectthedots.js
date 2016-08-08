81757a8482f9de6a02503a09fded5fd5220cdfce
Stephen Suen <ssuen@mit.edu>
2016-08-08 18:55:14 -0400

    Reorganized JS and implemented graph <=> table interactions.

--- databasic/static/js/connectthedots.js
+++ databasic/static/js/connectthedots.js
@@ -12,8 +12,8 @@
    * Adapted from https://bl.ocks.org/mbostock/4062045
    */
   function drawGraph(graph) {
-    var ticksElapsed = 0,
-        svg = d3.select('svg'),
+    // setup SVG dimensions
+    var svg = d3.select('svg'),
         container = d3.select('.ctd-container');
 
     var padding = parseFloat(container.style('padding-left').slice(0, -2)),
@@ -25,16 +25,18 @@
        .attr('height', height)
        .style('background-color', BACKGROUND_COLOR);
 
+    // setup simulation
     var simulation = d3.forceSimulation()
                        .force('charge', d3.forceManyBody())
                        .force('link', d3.forceLink().id(function(d) { return d.id; }))
                        .force('center', d3.forceCenter(width / 2, height / 2));
 
-    // Number of ticks before graph reaches equilibrium
-    var stableAt = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay()));
+    var ticksElapsed = 0,
+        stableAt = Math.ceil(
+                     Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())
+                   ); // number of ticks before graph reaches equilibrium
 
-    var progress = d3.select('.ctd-progress');
-
+    // setup graphical elements and bind data
     var edge = svg.append('g')
                   .classed('edges', true)
                   .style('display', 'none')
@@ -49,31 +51,68 @@
                                       .enter().append('circle')
                                       .attr('r', NODE_RADIUS)
                                       .attr('stroke-width', NODE_STROKE)
-                                      .attr('id', function(d) { return d.id; })
-                                      .on('mouseover', function(d) {
-                                        if (!activeNode) showTooltip(d);
-                                      })
-                                      .on('mouseout', function(d) {
-                                        if (!activeNode) tooltip.classed('in', false);
-                                      })
-                                      .on('click', setActiveNode)
-                                      .call(d3.drag().on('start', dragStart)
-                                                     .on('drag', dragUpdate)
-                                                     .on('end', dragEnd));
+                                      .attr('id', function(d) { return d.id; });
 
-    var tooltip = d3.select('.ctd-tooltip');
+    var progress = d3.select('.ctd-progress'),
+        tooltip = d3.select('.ctd-tooltip').style('display', 'none'),
+        tableRow = d3.selectAll('.ctd-table > tbody > tr').attr('id', function() {
+                                                            return this.getAttribute('data-id');
+                                                          });
 
-    var activeNode; // Currently selected node 
-    svg.on('click', function() {
-      if (d3.event.target.tagName !== 'circle') clearActiveNode();
-    });
+    // start simulation
+    simulation.nodes(graph.nodes)
+              .on('tick', tickUpdate)
+              .force('link').links(graph.links);
+
+    // event handlers for dragging and setting active node
+    var activeNode;
+    node.on('mouseover', mouseoverNode)
+        .on('mouseout', mouseoutNode)
+        .on('click', setActiveNode)
+        .call(d3.drag().on('start', dragStart)
+                       .on('drag', dragUpdate)
+                       .on('end', dragEnd));
+
+    svg.on('click', function() { if (d3.event.target.tagName !== 'circle') clearActiveNode(); });
+    tableRow.on('mouseover', function() { mouseoverNode(svg.select('#' + this.id).data()[0]) })
+            .on('mouseout', function() { mouseoutNode(svg.select('#' + this.id).data()[0]) })
+            .on('click', function() { setActiveNode(svg.select('#' + this.id).data()[0]) });
+
+    /**
+     * Highlight a node in the graph and table
+     */
+    function mouseoverNode(d) {
+      if (!activeNode) showTooltip(d);
+
+      node.filter(function(c) { return c.id === d.id; }).classed('hover', true);
+      tableRow.filter(function() { return this.id === d.id; }).classed('hover', true);
+    }
+
+    /**
+     * De-highlight a node in the graph and table
+     */
+    function mouseoutNode(d) {
+      if (!activeNode) tooltip.classed('in', false);
+
+      node.classed('hover', false);
+      tableRow.classed('hover', false);
+    }
 
     /**
      * Set the active node
      */
-    function setActiveNode(node) {
-      activeNode = node.id;
-      showTooltip(node);
+    function setActiveNode(d) {
+      if (activeNode !== d.id) {
+        activeNode = d.id;
+
+        node.classed('active', false);
+        tableRow.classed('active', false);
+
+        node.filter(function(d) { return d.id === activeNode; }).classed('active', true);
+        tableRow.filter(function() { return this.id === activeNode; }).classed('active', true);
+
+        showTooltip(d);
+      }
     }
 
     /**
@@ -81,14 +120,37 @@
      */
     function clearActiveNode() {
       activeNode = null;
+      node.classed('active', false);
+      tableRow.classed('active', false);
       tooltip.classed('in', false);
+    }
+
+    /**
+     * Handle node drag events
+     */
+    function dragStart(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+      setActiveNode(d);
+    }
+
+    function dragUpdate(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragEnd(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
     }
 
     /**
      * Show the tooltip for a particular node
      */
-    function showTooltip(node) {
-      tooltip.datum(node)
+    function showTooltip(d) {
+      tooltip.datum(d)
              .select('.tooltip-inner').html(function(d) {
                return '<strong>' + d.id + '</strong><br>Degree: ' + d.degree +
                '<br>Centrality: ' + parseFloat(d.centrality.toFixed(ROUNDING_PRECISION));
@@ -126,11 +188,12 @@
     function tickUpdate() {
       ticksElapsed++;
       if (ticksElapsed < stableAt) {
-        progress.style('width', 100 * ticksElapsed / stableAt + '%');
+        progress.style('width', width * ticksElapsed / stableAt + 'px');
       } else if (ticksElapsed === stableAt) {
-        progress.style('width', '100%');
+        progress.remove();
         rescaleGraph(BOUNDARY_PROPORTION);
         svg.selectAll('g').style('display', 'block');
+        tooltip.style('display', 'block');
       }
 
       node.attr('cx', function(d) { return d.x; })
@@ -158,34 +221,9 @@
       svg.selectAll('g')
          .attr('transform', 'translate(' + scale.dx + ', ' + scale.dy + ') scale(' + scale.factor + ')');
     }
-
-    /**
-     * Handle node drag events
-     */
-    function dragStart(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-      setActiveNode(d);
-    }
-
-    function dragUpdate(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragEnd(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
-    simulation.nodes(graph.nodes)
-              .on('tick', tickUpdate)
-              .force('link').links(graph.links);
   }
 
-  // Map link data (indices) to node ids
+  // map link data (indices) to node ids
   data.links.forEach(function(e) {
     e.source = data.nodes[e.source].id;
     e.target = data.nodes[e.target].id;
@@ -193,7 +231,7 @@
 
   drawGraph(data);
 
-  // Event handlers for export buttons
+  // event handlers for export buttons
   document.querySelector('#export-png').addEventListener('click', function() {
     saveSvgAsPng(document.querySelector('svg'), getFilename(filename, 'png'), {scale: 2.0});
   });
