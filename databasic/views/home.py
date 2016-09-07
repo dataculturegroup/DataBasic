3646ba6b75723b740ce14b07521ec7de22cdcc4c
Stephen Suen <ssuen@mit.edu>
2016-09-07 13:29:38 -0400

    Added news/blog section.

--- databasic/views/home.py
+++ databasic/views/home.py
@@ -1,4 +1,4 @@
-import logging
+import feedparser, logging, re, time
 from flask import Blueprint, render_template
 
 mod = Blueprint('home', __name__, url_prefix='/<lang_code>', template_folder='../templates/home')
@@ -7,4 +7,29 @@ logger = logging.getLogger(__name__)
 
 @mod.route('/')
 def index():
-    return render_template('index.html')
+    logger.info('Fetching blog posts...')
+
+    blog_feed = feedparser.parse('http://blog.databasic.io/feed/')
+    blog_posts = blog_feed['entries'][0:3]
+
+    databasic_news = []
+    for post in blog_posts:
+        regex = re.match('(<img .*? />)?(.*?&#8230;)', post['description'])
+
+        if regex.group(1) is not None:
+            image = re.search('(?<=src=").*?(?=")', regex.group(1)).group(0)
+        else:
+            image = None
+
+        logger.info('"%s" (%s)', post['title'], post['link'])
+
+        databasic_news.append({
+            'title': post['title'],
+            'author': post['author'],
+            'link': post['link'],
+            'published': time.strftime('%B %d, %Y', post['published_parsed']),
+            'description': regex.group(2),
+            'image': image
+        })
+
+    return render_template('index.html', databasic_news=databasic_news)
