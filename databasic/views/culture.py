78071cc18bcfcc259cbdb7508e203c172d09763c
esalling23 <esalling23@gmail.com>
2017-08-09 13:35:45 -0400

    add two new culture pages, add color themes to pages, add forms for each page

--- databasic/views/culture.py
+++ databasic/views/culture.py
@@ -1,6 +1,6 @@
 import feedparser, logging, re, time
 from collections import OrderedDict
-from databasic.forms import CultureForm, CultureSketchAStory
+from databasic.forms import CultureForm, CultureSketchAStory, CultureAskQuestions, CultureConvinceMe
 from flask import Blueprint, render_template, request
 
 mod = Blueprint('culture', __name__, url_prefix='/<lang_code>/culture', template_folder='../templates/culture')
@@ -18,6 +18,19 @@ def sketch():
 
   return render_template('sketch.html', form=form, tool_name='sketch')
 
+@mod.route('/convince-me')
+def convince():
+
+  form = CultureConvinceMe()
+
+  return render_template('convince.html', form=form, tool_name='convince')
+
+@mod.route('/ask-questions')
+def questions():
+
+  form = CultureAskQuestions()
+
+  return render_template('questions.html', form=form, tool_name='questions')
 
 @mod.route('/feedback', methods=['POST', 'GET'])
 def feedback():
