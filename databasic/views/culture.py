5ae0143a6de2e64dde12d409d9b68a23ef231169
Rahul Bhargava <rahulbot@gmail.com>
2017-09-19 09:39:19 -0400

    linting

--- databasic/views/culture.py
+++ databasic/views/culture.py
@@ -7,33 +7,32 @@ mod = Blueprint('culture', __name__, url_prefix='/<lang_code>/culture', template
 
 logger = logging.getLogger(__name__)
 
+
 @mod.route('/', methods=('GET', 'POST'))
 def index():
-  return render_template('culture.html', tool_name='culture')
+    return render_template('culture.html', tool_name='culture')
+
 
 @mod.route('/sketch-a-story')
 def sketch():
+    form = CultureSketchAStory()
+    return render_template('sketch.html', form=form, tool_name='sketch')
 
-  form = CultureSketchAStory()
-
-  return render_template('sketch.html', form=form, tool_name='sketch')
 
 @mod.route('/convince-me')
 def convince():
+    form = CultureConvinceMe()
+    return render_template('convince.html', form=form, tool_name='convince')
 
-  form = CultureConvinceMe()
-
-  return render_template('convince.html', form=form, tool_name='convince')
 
 @mod.route('/ask-questions')
 def questions():
+    form = CultureAskQuestions()
+    return render_template('questions.html', form=form, tool_name='questions')
 
-  form = CultureAskQuestions()
-
-  return render_template('questions.html', form=form, tool_name='questions')
 
 @mod.route('/feedback', methods=['POST', 'GET'])
 def feedback():
-  logger.info('what did we get')
-  logger.info(request)
-  return 'Nice'
+    logger.info('what did we get')
+    logger.info(request)
+    return 'Nice'
