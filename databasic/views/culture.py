9ac5f5634ad91b9333f404d9407867b158b3c2bc
Rahul Bhargava <rahulbot@gmail.com>
2018-02-24 13:30:31 -0500

    Remove unused forms from email-feedback feature

--- databasic/views/culture.py
+++ databasic/views/culture.py
@@ -1,8 +1,5 @@
 import logging
-from flask import Blueprint, render_template, request, jsonify
-
-from databasic.forms import CultureFeedbackForm, CultureSketchAStory, CultureAskQuestions, CultureConvinceMe
-from databasic.mail import send_email, DEFAULT_SENDER
+from flask import Blueprint, render_template
 
 mod = Blueprint('culture', __name__, url_prefix='/<lang_code>/culture', template_folder='../templates/culture')
 
@@ -11,57 +8,29 @@ logger = logging.getLogger(__name__)
 
 @mod.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template('culture.html', tool_name='culture')
+    return render_template('culture.html')
 
 
 @mod.route('/sketch-a-story')
 def sketch():
-    form = CultureSketchAStory()
-    return render_template('sketch.html', form=form, tool_name='sketch')
+    return render_template('sketch.html')
 
 
 @mod.route('/convince-me')
 def convince():
-    form = CultureConvinceMe()
-    return render_template('convince.html', form=form, tool_name='convince')
+    return render_template('convince.html')
 
 
 @mod.route('/ask-questions')
 def questions():
-    form = CultureAskQuestions()
-    return render_template('questions.html', form=form, tool_name='questions')
+    return render_template('questions.html')
 
 
 @mod.route('/build-a-sculpture')
 def sculpture():
-    form = CultureFeedbackForm()
-    return render_template('sculpture.html', form=form, tool_name='sculpture')
+    return render_template('sculpture.html')
 
 
 @mod.route('/connections')
 def connections():
-    form = CultureFeedbackForm()
-    return render_template('connections.html', form=form, tool_name='connectthedots')    
-
-
-@mod.route('/feedback', methods=['POST', 'GET'])
-def feedback():
-    try:
-        feedback = request.form['feedback']
-        from_email = request.form['email']
-        # now send an email
-        content = """
-    Hi,
-    
-    Thanks for your feedback!  We'll try to get back to you soon.
-    
-    Feedback:
-    """
-        content += feedback
-        send_email(DEFAULT_SENDER, [from_email, 'feedback@databasic.io'],
-                   "Data Culture Project: Feedback Received", content)
-    except Exception as e:
-        logger.error("email failed to send")
-        logger.exception(e)
-        return jsonify({'success': False})
-    return jsonify({'success': True})
+    return render_template('connections.html')    
