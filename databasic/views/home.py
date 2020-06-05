1c82904581b522cde80450a8f7b950d5b7fed529
Rahul Bhargava <rahulbot@gmail.com>
2020-06-05 09:15:31 -0400

    Remove blog post section and related dependencies, update PI affiliations

--- databasic/views/home.py
+++ databasic/views/home.py
@@ -1,39 +1,10 @@
-import feedparser, logging, re, time
+import logging
 from flask import Blueprint, render_template
 
 mod = Blueprint('home', __name__, url_prefix='/<lang_code>', template_folder='../templates/home')
 
 logger = logging.getLogger(__name__)
 
-
 @mod.route('/')
 def index():
-    logger.info('Fetching blog posts...')
-
-    blog_feed = feedparser.parse('http://blog.databasic.io/feed/')
-    blog_posts = blog_feed['entries'][0:3]
-
-    databasic_news = []
-    for post in blog_posts:
-        regex = re.match('(<img .*? />)?(.*?&#8230;)', post['description'])
-
-        image = None
-        description = None
-        if regex is not None:
-            if regex.group(1) is not None:
-                image = re.search('(?<=src=").*?(?=")', regex.group(1)).group(0)
-            if regex.group(2) is not None:
-                description = regex.group(2)
-
-        logger.info('"%s" (%s)', post['title'], post['link'])
-
-        databasic_news.append({
-            'title': post['title'],
-            'author': post['author'],
-            'link': post['link'],
-            'published': time.strftime('%B %d, %Y', post['published_parsed']),
-            'description': description,
-            'image': image
-        })
-
-    return render_template('index.html', databasic_news=databasic_news)
+    return render_template('index.html')
