c4357365098651637624dea45087f6ec05910ceb
Catherine D'Ignazio <kanarinka@gmail.com>
2021-03-14 09:22:29 -0400

    caching and encoding

--- scripts/init_samples.py
+++ scripts/init_samples.py
@@ -30,10 +30,12 @@ def init_samples():
             print("  Loading sample data file: {}".format(url))
             
             #reading files as utf-8 or else PROBLEMS 
+            #NOTE this does not fix the Rhodri Morgan file
             requests.encoding = 'utf-8'
             text = requests.get(url).text
 
             # write files as utf-8 or else PROBLEMS
+            #NOTE this does not fix the Rhodri Morgan file
             f = tempfile.NamedTemporaryFile(mode="w", delete=False, encoding='utf-8')
             f.write(text)
             f.close()
