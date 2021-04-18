f16db5d5df0bd0c49b8be87ce7c66a4e5d043053
Catherine D'Ignazio <kanarinka@gmail.com>
2021-04-18 11:26:00 -0400

    Welsh updates and language updates

--- translations-compile.sh
+++ translations-compile.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# CD NOTE - For some reason I have to run each of these two lines manually on my local machine for en_GB language
-# or else I get a strange locale error. 
-
 LANG=$1
 JSONFILE=databasic/translations/${LANG}/LC_MESSAGES/messages.json
 
