fb8df0227dc2b3d62582255095010ed0e0f67e6b
Joanna Leon <joanna.s.leon@gmail.com>
2026-03-11 16:07:18 -0400

    Allow translation compiling for singular languages; increased the nginx upload limit

--- translations-compile.sh
+++ translations-compile.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <language_code|all>"
+  exit 1
+fi
+
 LANG=$1
-JSONFILE=databasic/translations/${LANG}/LC_MESSAGES/messages.json
 
-pybabel compile -d databasic/translations
-pojson databasic/translations/${LANG}/LC_MESSAGES/messages.po > "${JSONFILE}"
+if [ "$LANG" = "all" ]; then
+  pybabel compile -d databasic/translations
 
-echo "compiled '${LANG}' translation"
+  for POFILE in databasic/translations/*/LC_MESSAGES/messages.po; do
+    JSONFILE="${POFILE%.po}.json"
+    pojson "$POFILE" > "$JSONFILE"
+  done
+
+  echo "compiled all translations"
+
+else
+  JSONFILE=databasic/translations/${LANG}/LC_MESSAGES/messages.json
+
+  pybabel compile -d databasic/translations -l "${LANG}"
+  pojson "databasic/translations/${LANG}/LC_MESSAGES/messages.po" > "${JSONFILE}"
+
+  echo "compiled '${LANG}' translation"
+fi
